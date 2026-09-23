cbfef647896210ce36ec3bee23af8de378d97512
xiyu.wxy <xiyu.wxy@alibaba-inc.com>
2026-09-23 09:57:46 +0000

    [ci] require authentication for Redis tests

--- kv_cache_manager/manager/test/meta_searcher_redis_test.cc
+++ kv_cache_manager/manager/test/meta_searcher_redis_test.cc
@@ -65,7 +65,8 @@ public:
     std::shared_ptr<MetaStorageBackendConfig> ConstructMetaStorageBackendConfig() {
         auto meta_storage_backend_config = std::make_shared<MetaStorageBackendConfig>();
         meta_storage_backend_config->SetStorageType(META_REDIS_BACKEND_TYPE_STR);
-        meta_storage_backend_config->SetStorageUri(redis_test::Uri("", redis_test::kMetaSearcherDb));
+        meta_storage_backend_config->SetStorageUri(
+            redis_test::Uri("test_user:test_password", redis_test::kMetaSearcherDb));
         return meta_storage_backend_config;
     }
 
